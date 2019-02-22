8de5f5444b6b1be6007586de1448936ad12f29f4
Felipe Lima Freire <felipe.lima.freire@concrete.com.br>
2019-02-22 08:40:16 -0300

    Adding tests for saving a list of telephones
    # Conflicts:
    #	src/main/kotlin/br/com/dev/users/model/User.kt

--- src/test/kotlin/br/com/dev/users/unitary/TestUnitary.kt
+++ src/test/kotlin/br/com/dev/users/unitary/TestUnitary.kt
@@ -1,8 +1,8 @@
 package br.com.dev.users.unitary
 
+import br.com.dev.users.model.Phone
 import br.com.dev.users.model.User
-import main.JavalinApp
-import main.br.com.dev.users.dao.UserDAO
+import main.br.com.dev.users.service.PhoneService
 import main.br.com.dev.users.service.UserService
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -10,10 +10,12 @@ import org.junit.jupiter.api.Test
 class TestUnitary {
 
     lateinit var userService: UserService
+    lateinit var phoneService: PhoneService
 
     @BeforeEach
     fun setUp() {
         userService = UserService()
+        phoneService = PhoneService()
     }
 
     @Test
@@ -25,5 +27,17 @@ class TestUnitary {
         userService.save(user2)
 
         assert(userService.emailExists(user2))
+    }
+
+    @Test
+    fun `when saving list of telephone ids exists and greater than zero`() {
+        val phone1 = Phone("11", "99999999")
+        val phone2 = Phone("11", "99999999")
+
+        val phones = listOf(phone1, phone2)
+        phoneService.save(phones)
+
+        assert(phones[0].id.toInt() > 0)
+        assert(phones[1].id.toInt() > 1)
     }
 }
